5d0f65669c827e3755f66825dc9e9b1f045f737c
Ted Cybulski <tcybulski@gmail.com>
2016-05-27 15:49:24 -0500

    added motif generating powershell script

--- ngs_nmer_extract.py
+++ ngs_nmer_extract.py
@@ -106,12 +106,12 @@ def output_motif_csv(fname, mot_idxs, ct_idxs, bad_chars=['A','-']):
     mot, nts = extract_motifs_and_bases(fname, mot_idxs, ct_idxs,
                                         bad_chars=bad_chars)
     with open(''.join(fname.split('.')[:-1])+'_mot.csv', 'w') as o_f:
-        o_f.write(', '.join(['motif']+['site_'+str(ct)
+        o_f.write(','.join(['motif']+['site_'+str(ct)
                                      for ct in ct_idxs]) + '\n')
         for m, n in zip(mot, nts):
             o_f.write('%s' % m)
             for base in n:
-                o_f.write(', %s' % base)
+                o_f.write(',%s' % base)
             o_f.write('\n')
     
 ############
