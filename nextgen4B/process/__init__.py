0a10911acf4ee8b9dd5d53e119756805afb8267d
Ted Cybulski <tcybulski@gmail.com>
2016-08-10 10:47:08 -0500

    added process\__init__

---
+++ nextgen4B/process/__init__.py
@@ -0,0 +1,11 @@
+import time
+import logging
+
+timestr = time.strftime("%Y%m%d-%H%M%S")
+logging.basicConfig(filename='ngs_'+timestr+'.log',
+    level=logging.DEBUG, format='%(asctime)s %(message)s')
+
+# Am I doing this right?
+import ./filter
+import ./multimer
+import ./sites
