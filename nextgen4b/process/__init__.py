b48d27ef2244bdf5b2817477af750ee38084f007
Ted Cybulski <tcybulski@gmail.com>
2016-11-30 10:13:06 -0600

    python 3 doesn't like relative imports in modules

--- nextgen4b/process/__init__.py
+++ nextgen4b/process/__init__.py
@@ -1,6 +1,6 @@
 # Am I doing this right?
 __all__ = ['filter', 'multimer', 'sites']
 
-import filter
-import multimer
-import sites
+import nextgen4b.process.filter
+import nextgen4b.process.multimer
+import nextgen4b.process.sites
--- nextgen4b/tools/__init__.py
+++ nextgen4b/tools/__init__.py
@@ -1,5 +1,10 @@
-from demux import demux_dataset
-from experiment_yaml import generate_exp_dict
+"""
+Ted Cybulski
+11/22/2016 (Althought this existed earlier...)
+"""
+
+from nextgen4b.tools.demux import demux_dataset
+from nextgen4b.tools.experiment_yaml import generate_exp_dict
 
 __all__ = ['demux_dataset',
            'generate_exp_dict']
