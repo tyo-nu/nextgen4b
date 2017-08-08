ef2171aac0684ed4965560e293da75f9447ebd90
Ted Cybulski <tcybulski@gmail.com>
2017-08-08 16:48:45 -0500

    reinstituted lower bound on alignment scores
    changed back for re-run of Alexandra's NGS9,10,11

--- nextgen4b/process/filter.py
+++ nextgen4b/process/filter.py
@@ -269,7 +269,7 @@ def len_filter(seqs, l_cutoff=40, u_cutoff=200, l_barcode=0):
 # Alignment Filtering
 #####################
 
-def alignment_filter(seqs, template, gapopen=10, gapextend=0.5, lo_cutoff=0,
+def alignment_filter(seqs, template, gapopen=10, gapextend=0.5, lo_cutoff=300,
                      hi_cutoff=1000, cleanup=True):
     text_logger = logging.getLogger(__name__+'.text_logger')
     text_logger.info('Started alignment-based filtering')
