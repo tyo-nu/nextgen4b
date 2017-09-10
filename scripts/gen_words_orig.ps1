eed090154d4625bf5b14b0b7143904c2d3c0cf32
Ted Cybulski <tcybulski@gmail.com>
2017-09-10 12:28:06 -0500

    removed old demo code
    Removing extraneous code before transfer to Tyo organization

--- scripts/gen_words_orig.ps1
+++
@@ -1,9 +0,0 @@
-$nmin = 16
-$nmax = 19
-
-for ($i=$nmin; $i -le $nmax; $i++) {
-    $fname = 'aln_seqs_run' + $i + '_exp' + $i + '.fa'
-    $wordname = 'words_' + $i '.txt'
-    
-    python D:\Google Drive\To File-Research Docs\Screening\nextgenAnalysis\nextgen4B\ngsMultiExtract.py $fname $wordname 30 45 61 76 91
-}
